24c797a415e251de52e255c08b53c090c644a925
kuzeyisiklari <67115977+kuzeyisiklari@users.noreply.github.com>
2020-06-21 14:47:25 +0200

    Update FirstClass.java
    conflict yaptik galiba

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,9 +5,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-		System.out.println("third commit from local repository");
+		System.out.println("conflict yapmak istiyoruz");
 		System.out.println("first commit from remote repository");
 
 	}
 
 }
+      
